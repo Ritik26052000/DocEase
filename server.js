a2106e686a8b137a97e29f0ff043ff5475e6c262
Ritik26052000 <sonwanianshu2000@gmail.com>
2024-09-02 13:00:40 +0530

    DocEase backend

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const connnetToDB = require("./config/db");
+const userRouter = require("./routes/userRoutes");
+const documentRouter = require("./routes/documents");
+const folderRouter = require("./routes/folderRoutes");
+const auth = require("./middleware/auth");
+
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+app.use("/api/auth", userRouter);
+app.use("/api/documents", auth, documentRouter);
+app.use("/api/folders", auth, folderRouter);
+
+const PORT = process.env.PORT || 5000;
+const db_url = process.env.MONGODB_URL;
+
+app.listen(PORT, async () => {
+  try {
+    connnetToDB(db_url);
+    console.log(`Server running on port http://localhost:${PORT}`);
+  } catch (err) {
+    console.log("error", err);
+  }
+});
